7d29c19d2a9602ae6381971927bcac9d289cf5c3
datnguyen60 <dat.nguyenthese80@gmail.com>
2024-07-19 17:07:18 +0700

    Automatic commit message

--- convert2.js
+++ convert2.js
@@ -5,7 +5,6 @@ import fetch from "node-fetch";
 
 const commitMessage = "Automatic commit message";
 
-// Hàm thực hiện một lệnh Git
 const runCommand = (command) => {
   return new Promise((resolve, reject) => {
     exec(command, (error, stdout, stderr) => {
@@ -18,14 +17,11 @@ const runCommand = (command) => {
   });
 };
 
-// Hàm chính
 const main = async () => {
   try {
-    // Đọc nội dung HTML từ file
     const html = fs.readFileSync("indexRaw.html", "utf8");
     const $ = cheerio.load(html);
 
-    // Fetch dữ liệu từ API
     const response = await fetch(
       "http://35.225.195.77:1337/api/message-of-the-day"
     );
@@ -34,22 +30,19 @@ const main = async () => {
     }
     const data = await response.json();
 
-    // Cập nhật nội dung HTML
     $("#motd-text").text(data.data.attributes.message);
     const content =
       $.html().split("<!-- endOfPushPart -->")[0] + "</body></html>";
     fs.writeFileSync("index.html", content, "utf8");
     console.log("Nội dung đã được ghi vào index.html");
 
-    // Thực hiện các lệnh Git
     await runCommand("git add .");
     await runCommand(`git commit -m "${commitMessage}"`);
-    await runCommand("git push origin"); // Thay 'main' bằng nhánh chính của bạn nếu cần
+    await runCommand("git push origin");
     console.log("Commit và push thành công!");
   } catch (error) {
     console.error("Có lỗi xảy ra:", error);
   }
 };
 
-// Chạy hàm chính
 main();
